a1600520987e0448169828fd4dde65c4cc0af1fe
Iagocanalejas <iagocanalejas@gmail.com>
2018-11-20 18:00:16 +0100

    Added web

---
+++ web/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import {Component} from '@angular/core';
+import {NgbCalendar, NgbDate} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+    hoveredDate: NgbDate;
+
+    fromDate: NgbDate;
+    toDate: NgbDate;
+
+    collapsed = true;
+
+    constructor(calendar: NgbCalendar) {
+        this.fromDate = calendar.getToday();
+        this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    }
+
+    onDateSelection(date: NgbDate) {
+        if (!this.fromDate && !this.toDate) {
+            this.fromDate = date;
+        } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+            this.toDate = date;
+        } else {
+            this.toDate = null;
+            this.fromDate = date;
+        }
+    }
+
+    isHovered(date: NgbDate) {
+        return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+    }
+
+    isInside(date: NgbDate) {
+        return date.after(this.fromDate) && date.before(this.toDate);
+    }
+
+    isRange(date: NgbDate) {
+        return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
+    }
+}
